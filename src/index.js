076dce1595d3ebebc25c0f3566d91cde929d69e8
gintu tom <gintutom@gmail.com>
2020-05-19 03:01:55 +0530

    add eventlistners

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-var http = require("http");
+let app = require("express")();
+let http = require("http").createServer(app);
+let io = require("socket.io")(http);
+let cors = require("cors");
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+app.use(cors());
+
+io.on("connection", socket => {
+  console.log("a user is connected");
+  socket.emit("news", { hello: "world" });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("my other event", data => {
+    console.log(data);
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
