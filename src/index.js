5db98474b305aa96dfae7996fce57b109787ccc4
gintu tom <gintutom@gmail.com>
2020-05-21 02:41:45 +0530

    fix update issue

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ let app = require("express")();
 let http = require("http").createServer(app);
 let io = require("socket.io")(http);
 let cors = require("cors");
+let uuid = require("uuid");
 
 app.use(cors());
 
 io.on("connection", socket => {
   console.log("a user is connected");
-  // socket.emit("news", { hello: "world" });
+
   socket.on("join", data => {
-    socket.broadcast.emit(`new user, ${data.name} has joined the chat`);
+    // socket.broadcast.emit("from server", {
+    //   message: `${data.name} has joined the chat`,
+    //   sentBy: "admin",
+    //   id: uuid.v4(),
+    //   room: data.room
+    // });
 
     console.log(data);
   });
@@ -18,8 +24,15 @@ io.on("connection", socket => {
     io.emit("from server", msg);
   });
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  socket.on("disconnect", name => {
+    // socket.broadcast.emit("from server", {
+    //   message: `${name} has joined the chat`,
+    //   sentBy: "admin",
+    //   id: uuid.v4(),
+    //   room: ""
+    // });
+
+    console.log("user is disonnected");
   });
 });
 
